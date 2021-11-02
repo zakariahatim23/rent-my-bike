f553f69db85f75c5130d8f48cc5bdb6ec5f10e32
abdellah mallouk <moulayam16@gmail.com>
2021-11-02 11:04:49 +0100

    Creation of pundit and database routes/controller/model

---
+++ app/controllers/bikes_controller.rb
@@ -0,0 +1,10 @@
+class BikesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_01_165537) do
+ActiveRecord::Schema.define(version: 2021_11_02_093323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bikes", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "location"
+    t.string "category"
+    t.decimal "price"
+    t.datetime "listed_at"
+    t.string "photo"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_bikes_on_user_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.decimal "price_total"
+    t.bigint "user_id", null: false
+    t.bigint "bike_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["bike_id"], name: "index_reservations_on_bike_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +53,7 @@ ActiveRecord::Schema.define(version: 2021_11_01_165537) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bikes", "users"
+  add_foreign_key "reservations", "bikes"
+  add_foreign_key "reservations", "users"
 end
